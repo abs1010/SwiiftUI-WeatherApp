18c7c8f6ee8297e8e0c3c21beb49e40def726f5d
Alan Silva <abs10@globomail.com>
2020-12-12 21:40:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiiftUI-WeatherApp
---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Alan Silva on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
